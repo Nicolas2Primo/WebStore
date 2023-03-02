16cc94fa116672c433b3877267373c8149d31c53
Nicolas Primo <nicolasprimo@gmail.com>
2023-03-02 11:22:02 -0300

    First

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="w-screen h-screen bg-black flex items-center justify-center">
+      <div className="text-white text-3xl font-bold">TESTE</div>
+    </div>
+  );
+};
+
+export default Home;
