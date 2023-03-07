896dda898a3b1e3dcc1e04d5107636851d62075a
Nicolas Primo <nicolasprimo@gmail.com>
2023-03-07 13:07:39 -0300

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="w-screen h-screen  flex items-center justify-center">
-      <div className="text-3xl font-bold">TESTE</div>
+      <div className="text-3xl font-bold">HOME PAGE</div>
     </div>
   );
 };
