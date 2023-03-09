ba1a6325418b24889a8f7ff4c5d89d97b7864c78
Nicolas Primo <nicolasprimo@gmail.com>
2023-03-09 15:45:20 -0300

    estrutura nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { BiMenu } from "react-icons/bi";
 
 const Navbar = () => {
   return (
-    <nav className="fixed w-full  h-14  border-4 border-red-500 flex items-center justify-center">
-      NAVBAR
+    <nav className="fixed w-full h-14 flex items-center justify-between bg-black text-white px-3 py-1">
+      <span className=" text-xl text-center font-bold">Store</span>
+      <BiMenu color="white" size={28}></BiMenu>
     </nav>
   );
 };
