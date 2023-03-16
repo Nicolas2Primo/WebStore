3a4718fc3f01b084162d7824e2912d5dcfa10f45
Nicolas Primo <nicolasprimo@gmail.com>
2023-03-16 12:04:17 -0300

    some optimizations

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,47 +2,53 @@ import React, { useState } from "react";
 import { BiMenu } from "react-icons/bi";
 import { FaShoppingCart } from "react-icons/fa";
 import MenuItems from "./MenuItems";
+import classNames from "classnames";
 
 const Navbar = () => {
-  const [menuItem, setMenuItem] = useState([
-    { nome: "teste1" },
-    { nome: "teste2" },
-    { nome: "teste3" },
-    { nome: "teste4" },
+  const [menuItems, setMenuItems] = useState([
+    "secção 1",
+    "secção 2",
+    "secção 3",
+    "secção 4",
   ]);
 
   const [open, setOpen] = useState(false);
 
-  const handleOpen = () => {
-    setOpen(!open);
-  };
+  const toggleMenu = () => setOpen(!open);
+
+  const renderMenuItems = (styleOption) =>
+    menuItems.map((item, index) => (
+      <MenuItems key={index} name={item} style_option={styleOption} />
+    ));
+
+  const navClass = classNames(
+    "z-10 fixed w-full h-14 flex items-center justify-between bg-black text-white px-3 py-1",
+  );
+
+  const mobileMenuClass = classNames(
+    "z-0 absolute w-screen h-52 flex flex-col gap-3 items-center justify-center bg-black transition-all duration-100 ease-in",
+    {
+      "mt-[17%]": open,
+      "mt-[-30%] invisible": !open,
+    },
+  );
 
   return (
     <div>
-      <nav className=" z-10 fixed w-full h-14 flex items-center justify-between bg-black text-white px-3 py-1">
-        <span className=" text-xl text-center font-bold">Store</span>
+      <nav className={navClass}>
+        <span className="text-xl text-center font-bold">Store</span>
         <div className="flex gap-4 items-center justify-center">
           <FaShoppingCart size={19} />
           <BiMenu
             color="white"
             size={28}
-            onClick={handleOpen}
-            className=" md:hidden"
-          ></BiMenu>
-          {menuItem.map((item, index) => (
-            <MenuItems key={index} name={item.nome} style_option={false} />
-          ))}
+            onClick={toggleMenu}
+            className="md:hidden"
+          />
+          {renderMenuItems(false)}
         </div>
       </nav>
-      <div
-        className={` z-0 absolute w-screen h-52 flex flex-col gap-3 items-center justify-center bg-black transition-all duration-100 ease-in ${
-          open ? "mt-[17%]" : "mt-[-30%] invisible"
-        }`}
-      >
-        {menuItem.map((item, index) => (
-          <MenuItems key={index} name={item.nome} style_option={true} />
-        ))}
-      </div>
+      <div className={mobileMenuClass}>{renderMenuItems(true)}</div>
     </div>
   );
 };
