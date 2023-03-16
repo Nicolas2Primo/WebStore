03f256d6bf1e8ad5e552041753007655064a81ed
Nicolas Primo <nicolasprimo@gmail.com>
2023-03-16 12:29:44 -0300

    more optimizations

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,20 +6,16 @@ import classNames from "classnames";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const [menuItems, setMenuItems] = useState([
-    "secção 1",
-    "secção 2",
-    "secção 3",
-    "secção 4",
-  ]);
+  const [state, setState] = useState({
+    menuItems: ["secção 1", "secção 2", "secção 3", "secção 4"],
+    open: false,
+  });
 
-  const [open, setOpen] = useState(false);
-
-  const toggleMenu = () => setOpen(!open);
+  const toggleMenu = () => setState({ ...state, open: !state.open });
 
   const renderMenuItems = (customClassName) =>
-    menuItems.map((item, index) => (
-      <MenuItems key={index} name={item} className={customClassName} />
+    state.menuItems.map((item, index) => (
+      <MenuItems key={index} name={item} className={`${customClassName}`} />
     ));
 
   const navbarClass =
@@ -34,8 +30,8 @@ const Navbar = () => {
   const mobileMenuClass = classNames(
     "z-0 absolute w-screen h-52 flex flex-col gap-3 items-center justify-center bg-black transition-all duration-100 ease-in",
     {
-      "mt-[17%]": open,
-      "mt-[-30%] invisible": !open,
+      "mt-[17%]": state.open,
+      "mt-[-30%] invisible": !state.open,
     },
   );
 
